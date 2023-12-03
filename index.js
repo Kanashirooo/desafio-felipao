6f71e1f50a31f3adab5fab8680305427220df58c
Kanashirooo <roger_kanashiro@hotmail.com>
2023-12-03 19:31:53 -0300

    Desafio Feito

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-let nomeDoHeroi = "Nome do Herói";
+let nomeDoHeroi = "Kanashirooo";
+let xpDoHeroi = 0;
+let nivel;
 
-for (let xpDoHeroi = 0; xpDoHeroi <= 12000; xpDoHeroi += 1000) {
-    let nivel;
+for (let xpDoHeroi = 0; xpDoHeroi <=12000; xpDoHeroi += 1000){
 
-    if (xpDoHeroi < 1000) {
-        nivel = "Ferro";
-    } else if (xpDoHeroi <= 2000) {
-        nivel = "Bronze";
-    } else if (xpDoHeroi <= 5000) {
-        nivel = "Prata";
-    } else if (xpDoHeroi <= 7000) {
-        nivel = "Ouro";
-    } else if (xpDoHeroi <= 8000) {
-        nivel = "Platina";
-    } else if (xpDoHeroi <= 9000) {
-        nivel = "Ascendente";
-    } else if (xpDoHeroi <= 10000) {
-        nivel = "Imortal";
-    } else {
-        nivel = "Radiante";
-    }
+if (xpDoHeroi <= 1000) {
+    nivel = "Ferro";
+} else if(xpDoHeroi <= 2000){
+    nivel = "Bronze";
+} else if(xpDoHeroi <=5000){
+    nivel = "Prata";
+} else if(xpDoHeroi <= 7000){
+    nivel = "Ouro";
+} else if ( xpDoHeroi <= 8000){
+    nivel="Platina";
+} else if(xpDoHeroi<= 9000){
+    nivel="Asendente";
+} else if(xpDoHeroi<=10000){
+    nivel="Imortal";
+} else {
+    nivel="Radiante";
+}
 
-    console.log(`O Herói de nome ${nomeDoHeroi} com ${xpDoHeroi} de XP está no nível de ${nivel}`);
+console.log("O Heroi de nome " + nomeDoHeroi + " está no nível de " + nivel);
 }
