e28df672397fe8a1a79d039b17410bc6da49bd94
Kanashirooo <roger_kanashiro@hotmail.com>
2023-12-03 19:02:49 -0300

    desafio feito

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log ("oi")
+let nomeDoHeroi = "Nome do Herói";
+
+for (let xpDoHeroi = 0; xpDoHeroi <= 12000; xpDoHeroi += 1000) {
+    let nivel;
+
+    if (xpDoHeroi < 1000) {
+        nivel = "Ferro";
+    } else if (xpDoHeroi <= 2000) {
+        nivel = "Bronze";
+    } else if (xpDoHeroi <= 5000) {
+        nivel = "Prata";
+    } else if (xpDoHeroi <= 7000) {
+        nivel = "Ouro";
+    } else if (xpDoHeroi <= 8000) {
+        nivel = "Platina";
+    } else if (xpDoHeroi <= 9000) {
+        nivel = "Ascendente";
+    } else if (xpDoHeroi <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${nomeDoHeroi} com ${xpDoHeroi} de XP está no nível de ${nivel}`);
+}
